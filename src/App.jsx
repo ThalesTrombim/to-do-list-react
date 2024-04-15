6b8fd171b18c77df2655f92ca721c8d9ac7e6357
thales <thalestr@outlook.com>
2024-04-15 17:14:08 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { TaskInput } from './components/TaskInput';
+import { TaskList } from './components/TaskList'
+
+function App() {
+  return (
+    <div>
+      <h1>Lista de tarefas</h1>
+
+      <TaskInput />
+      <TaskList />
+    </div> 
+  )
+}
+
+export default App;
---
+++ src/components/TaskInput.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const TaskInput = () => {
+  return (
+    <form>
+      <input type="text" placeholder='Adicione uma nova tarefa' />
+      <button type='submit'>Adicionar</button>
+    </form>
+  )
+}
