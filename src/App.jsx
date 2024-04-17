a55000abeae70b999dffa676a59c26265664ba27
thales <thalestr@outlook.com>
2024-04-17 09:09:20 -0300

    feat: add and delete tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+import { useState } from 'react';
 import { TaskInput } from './components/TaskInput';
 import { TaskList } from './components/TaskList'
 
 function App() {
+  const [tasks, setTask] = useState([]);
+
+  const addTask = (task) => {
+    setTask([...tasks, {id: Date.now(), text: task, done: false}])
+  }
+
+  const deleteTask = (taskId) => {
+    setTask(tasks.filter((task) => task.id !== taskId));
+  }
+
   return (
     <div>
       <h1>Lista de tarefas</h1>
 
-      <TaskInput />
-      <TaskList />
+      <TaskInput onAddTask={addTask} />
+      <TaskList tasks={tasks} onDeleteTask={deleteTask} />
     </div> 
   )
 }
--- src/components/TaskInput.jsx
+++ src/components/TaskInput.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export const TaskInput = () => {
+export const TaskInput = ({ onAddTask }) => {
+  const [input, setInput] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(input.trim()) {
+      onAddTask(input);
+      setInput("");
+    }
+  }
+
   return (
-    <form>
-      <input type="text" placeholder='Adicione uma nova tarefa' />
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text" 
+        placeholder='Adicione uma nova tarefa' 
+        value={input} 
+        onChange={(e) => setInput(e.target.value)}
+      />
       <button type='submit'>Adicionar</button>
     </form>
   )
